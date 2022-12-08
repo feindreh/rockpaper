7e87f54c1bc41fa0bf0363709dafc32eff61f439
feindreh <119032218+feindreh@users.noreply.github.com>
2022-12-08 14:33:41 +0100

    finish getComputerChoice

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -4,6 +4,18 @@ function getComputerChoice(){
     let ranNum = 0;
      ranNum = Math.floor(Math.random() * 3) + 1;      
     // switch with 3 possible returns
+    switch (ranNum) {
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        case 3:
+            return "scissors";
+            break;
+    };
+    console.log("ranNum Error");
 }
 
 
