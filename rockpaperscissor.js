c1f612f73c98591237ef2eae7fb9a18abad50e34
feindreh <119032218+feindreh@users.noreply.github.com>
2022-12-08 14:30:33 +0100

    add random number generator

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -1,5 +1,11 @@
 function getComputerChoice(){
 
     // make random 1 2 3
+    let ranNum = 0;
+     ranNum = Math.floor(Math.random() * 3) + 1;      
     // switch with 3 possible returns
 }
+
+
+
+console.log(getComputerChoice())
